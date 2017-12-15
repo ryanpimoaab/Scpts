104cde038c0704b3860998df207b584c3e2ef011
rayyanpivpn <rayyanpivpn@icloud.com>
2017-12-15 03:31:30 +0000

    Edit Fri 15 Dec 2017 03:31:29 GMT

---
+++ Scripts/SB/SB-hsts.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | sed 's/^0.0.0.0[ \t]*//' | sed '1,67d' | sed '/piratecams.com/d' | sed '/myfreecams.com/d' | sed '/mydirtystuff.com/d' | sed '/motherless.com/d' | awk '!x[$0]++' | awk '{print tolower($0)}' > /Users/rayyan/Github/Scrpts/Lists/SB-hsts.txt
+
+cp /Users/rayyan/Github/Scrpts/Lists/SB-hsts.txt /Users/rayyan/Github/SB/SB-hsts.txt
