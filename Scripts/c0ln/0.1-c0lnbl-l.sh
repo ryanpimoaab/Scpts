87e0295494fb134b78af08b13520ae6eae6e83e3
rayyanpivpn <rayyanpivpn@icloud.com>
2017-12-19 17:21:36 +0000

    Edit Tue 19 Dec 2017 17:21:36 GMT

--- Scripts/c0ln/0.1-c0lnbl-l.sh
+++ Scripts/c0ln/0.1-c0lnbl-l.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl -s https://raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/list.txt | sed '/poloniex.com/d' | sed '/coinbase/d' | awk '!x[$0]++' | awk '{print tolower($0)}' > /Users/rayyan/Github/Scrpts/Lists/0.1-c0lnbl-l.txt
+curl -s https://raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/list.txt | awk '!/bittrex.com|bitcoin.com|bitconnect.co|binance.com|poloniex.com|coinbase.com|bitaccess.co/' | awk '!x[$0]++' | awk '{print tolower($0)}' > /Users/rayyan/Github/Scrpts/Lists/0.1-c0lnbl-l.txt
 
 cp /Users/rayyan/Github/Scrpts/Lists/0.1-c0lnbl-l.txt /Users/rayyan/Github/c0ln/0.1-c0lnbl-l.txt
