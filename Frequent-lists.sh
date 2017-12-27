4986a5463b4c526d6e1e3c0be16b812ab0e55a83
rayyanpivpn <rayyanpivpn@icloud.com>
2017-12-27 19:29:43 +0000

    Edit Wed 27 Dec 2017 19:29:43 GMT

--- Frequent-lists.sh
+++ Frequent-lists.sh
@@ -1,81 +1,81 @@
 #! /bin/bash
 
 cd /Users/rayyan/Github/mwd-id
-[ -s hsts.txt ] || /Users/rayyan/Github/Scrpts/Scripts/mwd-id/hsts.sh
+[ -s hsts.txt ] || /Users/rayyan/Github/Scrpts/Scrpts/mwd-id/hsts.sh
 
 echo -e "\033[1;31m AgD \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/AgD/
+cd /Users/rayyan/Github/Scrpts/Scrpts/AgD/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m c0ln \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/c0ln/
+cd /Users/rayyan/Github/Scrpts/Scrpts/c0ln/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m h-f \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/h-f/
+cd /Users/rayyan/Github/Scrpts/Scrpts/h-f/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m dc-me \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/dc-me/
+cd /Users/rayyan/Github/Scrpts/Scrpts/dc-me/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m rw-list \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/rw-list/
+cd /Users/rayyan/Github/Scrpts/Scrpts/rw-list/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m SB-s \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/SB-s/
+cd /Users/rayyan/Github/Scrpts/Scrpts/SB-s/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m smaller-lists \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/smaller-lists_frequent/
+cd /Users/rayyan/Github/Scrpts/Scrpts/smaller-lists_frequent/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m uB-Ag \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/uB-Ag/
+cd /Users/rayyan/Github/Scrpts/Scrpts/uB-Ag/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m win \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/win/
+cd /Users/rayyan/Github/Scrpts/Scrpts/win/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m EZList \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/EZList
+cd /Users/rayyan/Github/Scrpts/Scrpts/EZList
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m Prgnt \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/Prgnt
+cd /Users/rayyan/Github/Scrpts/Scrpts/Prgnt
 for f in *.sh; do
 bash "$f" -H
 done
--- Weekly-lists.sh
+++ Weekly-lists.sh
@@ -2,73 +2,73 @@
 
 echo -e "\033[1;31m mw-id \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/mwd-id/
+cd /Users/rayyan/Github/Scrpts/Scrpts/mwd-id/
 for f in *.sh; do
 bash "$f" -H
 done
 
 cd /Users/rayyan/Github/mwd-id
-[ -s hsts.txt ] || /Users/rayyan/Github/Scrpts/Scripts/mwd-id/hsts.sh
+[ -s hsts.txt ] || /Users/rayyan/Github/Scrpts/Scrpts/mwd-id/hsts.sh
 
 echo -e "\033[1;31m SB \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/SB/
+cd /Users/rayyan/Github/Scrpts/Scrpts/SB/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m Weekly \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/weekly/
+cd /Users/rayyan/Github/Scrpts/Scrpts/weekly/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m h-f-ad \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/h-f-ad/
+cd /Users/rayyan/Github/Scrpts/Scrpts/h-f-ad/
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m h-f-emd \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/h-f-emd
+cd /Users/rayyan/Github/Scrpts/Scrpts/h-f-emd
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m h-f-psh \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/h-f-psh
+cd /Users/rayyan/Github/Scrpts/Scrpts/h-f-psh
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m h-f-fsa \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/h-f-fsa
+cd /Users/rayyan/Github/Scrpts/Scrpts/h-f-fsa
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m nope \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/nope
+cd /Users/rayyan/Github/Scrpts/Scrpts/nope
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m sycl-cmln \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/sycl-cmln
+cd /Users/rayyan/Github/Scrpts/Scrpts/sycl-cmln
 for f in *.sh; do
 bash "$f" -H
 done
 
 echo -e "\033[1;31m Prgnt-psh \033[0m"
 
-cd /Users/rayyan/Github/Scrpts/Scripts/Prgnt-psh
+cd /Users/rayyan/Github/Scrpts/Scrpts/Prgnt-psh
 for f in *.sh; do
 bash "$f" -H
 done
