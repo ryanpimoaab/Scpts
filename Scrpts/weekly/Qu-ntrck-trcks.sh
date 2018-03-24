7698cc2bf462a91863f02c0a2c0f9434ff35d05d
rayyanpivpn <rayyanpivpn@icloud.com>
2018-03-24 22:30:19 +0000

    Edit Sat 24 Mar 2018 22:30:19 GMT

--- Scrpts/weekly/Qu-ntrck-trcks.sh
+++ Scrpts/weekly/Qu-ntrck-trcks.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl -s https://raw.githubusercontent.com/quidsup/notrack/master/trackers.txt | sed 's/^||//' | sed '/^\#/d; s/[[:space:]]\#.*$//g; /^\!/d; s/[[:space:]]\!.*$//g; /^\./d; /\.$/d' | cut -d'^' -f-1 | awk '!x[$0]++' | awk '{print tolower($0)}' > /Users/rayyan/Github/Scrpts/Lists/Qu-ntrck-trcks.txt
+curl -s https://raw.githubusercontent.com/quidsup/notrack/master/trackers.txt | sed 's/^||//' | sed '/^\#/d; s/[[:space:]]\#.*$//g; /^\!/d; s/[[:space:]]\!.*$//g; /^\./d; /\.$/d' | cut -d'^' -f-1 | awk '!x[$0]++' | awk '{print tolower($0)}' | awk '!/imgclick.net/' > /Users/rayyan/Github/Scrpts/Lists/Qu-ntrck-trcks.txt
 
 cp /Users/rayyan/Github/Scrpts/Lists/Qu-ntrck-trcks.txt /Users/rayyan/Github/weekly/Qu-ntrck-trcks.txt
