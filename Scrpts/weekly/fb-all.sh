4c1b877080da90471ca24ad3228a9b6facee3a2a
rayyanpivpn <rayyanpivpn@icloud.com>
2018-05-23 21:24:25 +0100

    Edit Wed 23 May 2018 21:24:24 BST

--- Scrpts/weekly/fb-all.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-curl -s https://raw.githubusercontent.com/jmdugan/blocklists/master/corporations/facebook/all | sed 1,18d | awk '!x[$0]++' | awk '{print tolower($0)}' | sed 's/^0.0.0.0[ \t]*//' > /Users/rayyan/Github/Scrpts/Lists/fb-all.txt
-
-cp /Users/rayyan/Github/Scrpts/Lists/fb-all.txt /Users/rayyan/Github/weekly/fb-all.txt
