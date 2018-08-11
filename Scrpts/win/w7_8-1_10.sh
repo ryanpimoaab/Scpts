76924a5634b49a15832bb41844c70b12f6cd32bf
ryanpimoaab <ryanpimoaab@icloud.com>
2018-08-11 23:12:08 +0100

    Edit Sat 11 Aug 2018 23:12:08 BST

--- Scrpts/win/w7_8-1_10.sh
+++ Scrpts/win/w7_8-1_10.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl -s https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win7/spy.txt https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win81/spy.txt https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/win10/spy.txt | sed 's/^0.0.0.0[ \t]*//' | sed 1,3d |  cut -d'^' -f-1 | awk '!x[$0]++' | awk '{print tolower($0)}' > /Volumes/Rayyan/Pi/Github/Scrpts/Lists/w7_8-1_10.txt
+curl -s https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt | sed 's/^0.0.0.0[ \t]*//' | sed 1,3d |  cut -d'^' -f-1 | awk '!x[$0]++' | awk '{print tolower($0)}' > /Volumes/Rayyan/Pi/Github/Scrpts/Lists/w7_8-1_10.txt
 
 cp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/w7_8-1_10.txt /Volumes/Rayyan/Pi/Github/win/w7_8-1_10.txt
