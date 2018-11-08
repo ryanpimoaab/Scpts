41bf034a39f3a7c49fc79fde8b27e2f9361380d3
ryanpimoaab <ryanpimoaab@icloud.com>
2018-11-08 04:18:22 +0000

    Edit Thu 8 Nov 2018 04:18:21 GMT

--- Scrpts/uB-Ag/uB-as.sh
+++ Scrpts/uB-Ag/uB-as.sh
@@ -2,4 +2,4 @@
 
 curl -s https://raw.githubusercontent.com/byaka/ublock-antiskimming-list/master/build/data.txt | egrep '^\|\|' | cut -d'/' -f1 | cut -d '^' -f1 | cut -d '$' -f1 | tr -d '|' | awk '!x[$0]++' | awk '{print tolower($0)}' > /Volumes/Rayyan/Pi/Github/Scrpts/Lists/uB-as.txt
 
-cp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/uB-p.txt > /Volumes/Rayyan/Pi/Github/uB-Ag/uB-as.txt
+cp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/uB-as.txt /Volumes/Rayyan/Pi/Github/uB-Ag/uB-as.txt
