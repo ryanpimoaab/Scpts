0482cfd9a8db71f6309d3cbddc39c52a4101cba5
ryanpimoaab <ryanpimoaab@icloud.com>
2018-12-02 17:40:57 +0000

    Edit Sun 2 Dec 2018 17:40:57 GMT

--- Scrpts/SB/SB-hsts.sh
+++ Scrpts/SB/SB-hsts.sh
@@ -2,6 +2,6 @@
 
 curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | sed 's/^0.0.0.0[ \t]*//' | sed '1,67d' | sed '/piratecams.com/d' | sed '/connect.facebook.net/d' | sed '/myfreecams.com/d' | sed '/mydirtystuff.com/d' | sed '/camsoda.com/d' | sed '/motherless.com/d' | awk '!x[$0]++' | awk '{print tolower($0)}' > /Volumes/Rayyan/Pi/Github/Scrpts/Lists/SB-hsts.txt
 
-awk '!/.at$|.bid$|.cn$|.kim$|.li$|.link$|.pw$|.review$|.ro$|.ru$|.su$|.tk$|.top$|.ua$|.us$|.ve$|.work$|.ch$|.lol$|.v$|.w$|.loan$|.click$|.xyz$/ {print}' /Volumes/Rayyan/Pi/Github/Scrpts/Lists/SB-hsts.txt > tmp && mv tmp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/SB-hsts.txt
+awk '!/.at$|.bid$|.cn$|.kim$|.li$|.link$|.pw$|.review$|.ro$|.ru$|.su$|.tk$|.top$|.ua$|.ve$|.work$|.ch$|.lol$|.v$|.w$|.loan$|.click$/ {print}' /Volumes/Rayyan/Pi/Github/Scrpts/Lists/SB-hsts.txt > tmp && mv tmp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/SB-hsts.txt
 
 cp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/SB-hsts.txt /Volumes/Rayyan/Pi/Github/SB/SB-hsts.txt
--- Scrpts/tspr-frd-ph/tspr-frd-ph.sh
+++ Scrpts/tspr-frd-ph/tspr-frd-ph.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-curl -s https://tspprs.com/dl/fraud https://tspprs.com/dl/phishing | awk '{$1=$1}1' | awk '!x[$0]++' | awk '{print tolower($0)}' | awk '/apple/ || /google/ || /playstation/ || /samsung/ || /microsoft/ || /halifax/ || /hsbc/ || /barclay/ || /natwest/ || /netflix/ || /reddit/ || /payp/ || /ebay/ || /itunes/ || /cloud/ || /crypt/ || /whatsap/ || /psn/ || /mac/ || /ios/ || /chrome/ || /safari/ || /droid/ || /amazon/ || /twitch/ || /flash/ || /adobe/ || /linux/ || /coin/ || /bitt/ || /bank/ || /facebook/ || /insta/ || /twitter/ || /snapchat/ || /down/ || /update/{print}' > /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt
+curl -s https://tspprs.com/dl/fraud https://tspprs.com/dl/phishing | awk '{$1=$1}1' | awk '!x[$0]++' | awk '{print tolower($0)}' | awk '/apple/ || /google/ || /samsung/ || /microsoft/ || /halifax/ || /hsbc/ || /barclay/ || /natwest/ || /paypa/ || /ebay/ || /whatsap/ || /chrome/ || /android/ || /flash/ || /adobe/ || /bitcoin/ || /bank/ || /twitter/ || /downl/ || /down1/ || /icloud/{print}' > /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt
 
-awk '!/.at$|.bid$|.cn$|.kim$|.li$|.link$|.pw$|.review$|.ro$|.ru$|.su$|.tk$|.top$|.ua$|.us$|.ve$|.work$|.ch$|.lol$|.v$|.w$|.loan$|.click$|.xyz$/ {print}' /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt > tmp && mv tmp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt
+awk '!/.at$|.bid$|.cn$|.kim$|.li$|.link$|.pw$|.review$|.ro$|.ru$|.su$|.tk$|.top$|.ua$|.ve$|.work$|.ch$|.lol$|.v$|.w$|.loan$|.click$/ {print}' /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt > tmp && mv tmp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt
 
 
 cp /Volumes/Rayyan/Pi/Github/Scrpts/Lists/tspr-frd-ph.txt /Volumes/Rayyan/Pi/Github/tspr-frd-ph/tspr-frd-ph.txt
