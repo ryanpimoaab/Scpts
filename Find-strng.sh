698cc320f5f5d352feb3a13735e9e27a8f96dfb9
ryanpimoaab <ryanpimoaab@icloud.com>
2019-06-08 22:02:52 +0100

    Edit Sat 8 Jun 2019 22:02:52 BST

--- Find-strng.sh
+++ Find-strng.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /Volumes/Rayyan/Pi/Github/Scrpts/Lists
-grep -rnw '/Volumes/Rayyan/Pi/Github/Scrpts/Lists' -e 'solvemedia.com'
+grep -rnw '/Volumes/Rayyan/Pi/Github/Scrpts/Lists' -e 'track.locsci.io'
